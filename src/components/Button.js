dca20a7ebd863b8e98abc7852a911ec3db988aff
Jessica Seo <jessicaseo83@gmail.com>
2020-07-06 15:45:13 -0400

    Building a button

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,29 @@ import React from "react";
 
 import "components/Button.scss";
 
+import classnames from "classnames";
+
 export default function Button(props) {
-   return <></>;
+   // let buttonClass = "button";
+
+   // if (props.confirm) {
+   //    buttonClass += " button--confirm";
+   // }
+
+   // if (props.danger) {
+   //    buttonClass += " button--danger";
+   // }
+   const buttonClass = classnames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   });
+
+   return (<button 
+            className={buttonClass}
+            onClick={props.onClick}
+            disabled={props.disabled}
+         >
+            {props.children}
+         </button>
+   );
 }
