06a44516c8ac107e9e961b7c88e0d60f90c4f9c4
Jessica Seo <jessicaseo83@gmail.com>
2020-07-09 13:02:53 -0400

    Implement Promise.all and delete setDays function

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,56 +2,9 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import DayList from "components/DayList"
 import Appointment from "components/Appointment"
+import { getAppointmentsForDay } from "helpers/selectors"
 import "components/Application.scss";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-    interview: {
-      student: "Hannibal Lecter",
-      interviewer: {
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 4,
-    time: "4pm",
-    interview: {
-      student: "Peter Bishop",
-      interviewer: {
-        id: 2,
-        name: "Tori Malcolm",
-        avatar: "https://i.imgur.com/Nmx0Qxo.png" ,
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "5pm",
-    interview: null,
-  }
-
-];
 
 export default function Application(props) {
   const [state, setState] = useState({
@@ -60,19 +13,23 @@ export default function Application(props) {
     appointments: {}
   });
   const setDay = day => setState({ ...state, day });
-  const setDays = days => setState(prev => ({...prev, days}));
 
   useEffect(() => {
-    axios
-      .get('api/days')
-      .then(response => setDays(response.data))
-      .catch(error => {
-        console.log(error.response.status);
-        console.log(error.response.headers);
-        console.log(error.response.data);
-      })
-  },[])
 
+    Promise.all([
+      axios.get('api/days'),
+      axios.get('api/appointments')
+    ]).then((all) => {
+      setState(prev => ({ days: all[0].data, appointments: all[1].data}))
+    }).catch(error => {
+      console.log(error.response.status);
+      console.log(error.response.headers);
+      console.log(error.response.data);
+    })
+
+  },[])
+  
+  const appointments = getAppointmentsForDay(state, state.day)
   const schedule = appointments.map(appointment => {
 
     return (
