003bc1f57146fdf8ea516cdf0c4dbaf229d903a2
Jessica Seo <jessicaseo83@gmail.com>
2020-07-09 13:53:45 -0400

    Implement getInterview function

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -9,6 +9,20 @@ export function getAppointmentsForDay(state, day) {
 
   const AppointmentOfTheDay = whichDay.appointments.map(appointmentId => state.appointments[appointmentId])
   
-  return AppointmentOfTheDay
+  return AppointmentOfTheDay;
 
+}
+
+export function getInterview(state, interview) {
+
+  if (!interview) {
+    return null;
+  }
+
+  const interviewResult = {
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer]
+  }
+
+  return interviewResult;
 }
