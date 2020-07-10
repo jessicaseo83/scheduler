96a948bc11b9b44393c22fccf3b6a3448007a89c
Jessica Seo <jessicaseo83@gmail.com>
2020-07-10 19:46:07 -0400

    Implement save function and SAVING mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -3,19 +3,34 @@ import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
 import useVisualMode from "hooks/useVisualMode";
+import Form from "components/Appointment/Form";
+import Status from "components/Appointment/Status";
+
 import "components/Appointment/styles.scss";
-import Form from "./Form";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
+const SAVING = "SAVING";
 
 export default function Appointment (props) {
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
+  function save(name, interviewer) {
 
+    transition(SAVING);
+
+    const interview = {
+      student: name,
+      interviewer
+    };
+    
+    props.bookInterview(props.id, interview)
+    transition(SHOW)
+    
+  }
 
   return (
     <article className="appointment">
@@ -31,8 +46,10 @@ export default function Appointment (props) {
         <Form
           interviewers={props.interviewers}
           onCancel={back}
+          onSave={save}
         />
       )}
+      {mode === SAVING && (<Status message="Saving..."/>)}
 
     </article>
   )
