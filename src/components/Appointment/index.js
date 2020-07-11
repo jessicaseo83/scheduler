c218f51dc585ba53823d583908a86a94a687dd28
Jessica Seo <jessicaseo83@gmail.com>
2020-07-11 11:10:51 -0400

    Implement EDIT mode

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,11 +7,15 @@ import Form from "components/Appointment/Form";
 import Status from "components/Appointment/Status";
 
 import "components/Appointment/styles.scss";
+import Confirm from "./Confirm";
 
 const EMPTY = "EMPTY";
 const SHOW = "SHOW";
 const CREATE = "CREATE";
 const SAVING = "SAVING";
+const DELETING = "DELETING";
+const CONFIRM = "CONFIRM";
+const EDIT = "EDIT";
 
 export default function Appointment (props) {
   const { mode, transition, back } = useVisualMode(
@@ -26,11 +30,22 @@ export default function Appointment (props) {
       student: name,
       interviewer
     };
-    
     props.bookInterview(props.id, interview)
-    transition(SHOW)
+    setTimeout(() => {
+      transition(SHOW);
+    }, 1000)
     
+  };
+
+  function deleteAppointment() {
+    transition(DELETING);
+    props.cancelInterview(props.id)
+    setTimeout(() => {
+      transition(EMPTY);
+    }, 1000)
+    console.log("delete")
   }
+
 
   return (
     <article className="appointment">
@@ -40,6 +55,8 @@ export default function Appointment (props) {
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer}
+          onDelete={() => transition(CONFIRM)}
+          onEdit={() => transition(EDIT)}
         />
       )}
       {mode === CREATE && (
@@ -49,7 +66,24 @@ export default function Appointment (props) {
           onSave={save}
         />
       )}
-      {mode === SAVING && (<Status message="Saving..."/>)}
+      {mode === SAVING && (<Status message="Saving"/>)}
+      {mode === DELETING && (<Status message="Deleting"/>)}
+      {mode === CONFIRM && (
+        <Confirm
+          onCancel={back}
+          onConfirm={deleteAppointment}
+          message="Are you sure you would like to delete?"
+        />
+      )}
+      {mode === EDIT && (
+        <Form
+          name={props.interview.student}
+          interviewer={props.interview.interviewer}
+          interviewers={props.interviewers}
+          onSave={save}
+          onCancel={back}
+        />
+      )}
 
     </article>
   )
