9a3ce07a517e0113d780d1819ebd7fd7b28afc71
Jessica Seo <jessicaseo83@gmail.com>
2020-07-13 17:09:13 -0400

    Add validate function and update onSave

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,6 +5,7 @@ import Button from "components/Button";
 export default function Form(props) {
   const [name, setName] = useState(props.name || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
+  const [error, setError] = useState("");
 
   const reset = () => {
     setName("");
@@ -15,6 +16,14 @@ export default function Form(props) {
     reset();
     props.onCancel();
 
+  }
+
+  function validate() {
+    if (name === "") {
+      setError("Student name cannot be blank");
+      return;
+    }
+    props.onSave(name, interviewer);
   }
 
   return (
@@ -28,15 +37,16 @@ export default function Form(props) {
             type="text"
             placeholder="Enter Student Name"
             onChange={event => setName(event.target.value)}
-           
+            data-testid="student-name-input"
           />
+        <section className="appointment__validation">{error}</section>
         </form>
         <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={event => props.onSave(name, interviewer)}>Save</Button>
+          <Button confirm onClick={validate}>Save</Button>
         </section>
       </section>
     </main>
