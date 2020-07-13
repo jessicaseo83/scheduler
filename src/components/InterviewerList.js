d00f22907265fd7ec3446bc696287470267ecb6a
Jessica Seo <jessicaseo83@gmail.com>
2020-07-13 16:51:26 -0400

    Add Type Checking with Prop Types

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,6 +1,7 @@
 import React from  "react";
 import InterviewerListItem from "components/InterviewerListItem";
 import "components/InterviewerList.scss";
+import PropTypes from 'prop-types';
 
 export default function InterviewerList (props) {
 
@@ -25,3 +26,9 @@ export default function InterviewerList (props) {
   
   );
 }
+
+//Type Checking with Prop Types
+InterviewerList.propTypes = {
+  value: PropTypes.number,
+  onChange: PropTypes.func.isRequired
+};
