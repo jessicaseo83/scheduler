bcf68e0aeb0d3d6f3bcc7a33a924e8d798741fa3
Jessica Seo <jessicaseo83@gmail.com>
2020-07-14 17:54:58 -0400

    Implement axios.put

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -56,6 +56,7 @@ const fixtures = {
 export default {
   defaults: { baseURL: ""},
   get: jest.fn(url => {
+    
     if (url === "/api/days") {
       return Promise.resolve({
         status: 200,
@@ -77,5 +78,13 @@ export default {
         data: fixtures.interviewers
       })
     }
+  }),
+
+  put: jest.fn(url => {
+    return Promise.resolve({
+      status: 204,
+      statusText: "No Content"
+    })
   })
+
 }
